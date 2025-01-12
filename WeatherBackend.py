a065591345f485ebe11904c7198cc519d8dec7d9
gayanrajapakshage <gayanr17@gmail.com>
2025-01-11 20:24:16 -0500

    Upload Project

---
+++ WeatherBackend.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
+
+# Create app
+app = Flask(__name__)
+CORS(app, resources={r"/current-weather": {"origins": "http://localhost:3000"}})
+CORS(app, resources={r"/weekly-forecast": {"origins": "http://localhost:3000"}})
+CORS(app, resources={r"/search-city": {"origins": "http://localhost:3000"}})
+
+# API key
+WEATHER_API_KEY = "ae3e3e2375f74ca381d11445242904"  # Replace with your valid API key
+WEATHER_API_URL = "http://api.weatherapi.com/v1"
+
+# Define search city endpoint
+@app.route("/search-city", methods=["GET"])
+def search_city():
+    query = request.args.get("query")
+    if not query:
+        return jsonify({"error": "Please provide a search query."}), 400
+    
+    # Request city search data
+    response = requests.get(
+        f"{WEATHER_API_URL}/search.json",
+        params={"key": WEATHER_API_KEY, "q": query},
+    )
+    
+    if response.status_code != 200:
+        return jsonify({"error": "Could not get city data."}), response.status_code
+    
+    data = response.json()
+    return jsonify(data)
+
+# Define current weather endpoint
+@app.route("/current-weather", methods=["GET"])
+def current_weather():
+    city = request.args.get("city")
+    if not city:
+        return jsonify({"error": "Please provide a city name."}), 400
+    
+    # Request current weather data
+    response = requests.get(
+        f"{WEATHER_API_URL}/current.json",
+        params={"key": WEATHER_API_KEY, "q": city, "aqi": "no"},
+    )
+    
+    if response.status_code != 200:
+        return jsonify({"error": "Could not get weather data."}), response.status_code
+    
+    data = response.json()
+    return jsonify(data)
+
+# Define weekly forecast endpoint
+@app.route("/weekly-forecast", methods=["GET"])
+def weekly_forecast():
+    city = request.args.get("city")
+    if not city:
+        return jsonify({"error": "Please provide a city name."}), 400
+    
+    # Request weekly forecast data
+    response = requests.get(
+        f"{WEATHER_API_URL}/forecast.json",
+        params={"key": WEATHER_API_KEY, "q": city, "days": 7},
+    )
+    
+    if response.status_code != 200:
+        return jsonify({"error": "Could not get forecast data."}), response.status_code
+    
+    data = response.json()
+    return jsonify(data)
+
+# Server Flask App
+if __name__:
+    app.run(host='0.0.0.0', port=5001, debug=True)  
---
+++ src/App.js
@@ -0,0 +1,134 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+//import Select from 'react-select';
+
+function App() {
+  const [city, setCity] = useState(''); // State to hold the city input value
+  const [weather, setWeather] = useState(null); // State to hold weather data
+  const [error, setError] = useState(null); // State to hold error messages
+  const [weeklyForecast, setWeeklyForecast] = useState(null);
+  const [searchCity, setSearchCity] = useState(null);
+
+  const getWeather = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:5001/current-weather', {
+        params: { city: city },
+      });
+      setWeather(response.data);
+      setError(null);
+  } catch (error) {
+    setError("Sorry, we couldn't find that city. Please try again with a different city name.");
+    setWeather(null);
+  }
+  };
+
+  const getWeeklyForecast = async() => {
+    try{
+      const response = await axios.get('http://127.0.0.1:5001/weekly-forecast', {
+        params: { city: city },
+      });
+      console.log('Weekly forecast data:', response.data);
+      setWeeklyForecast(response.data);
+      setError(null);
+    } catch(error){
+      setWeeklyForecast(null);
+    }
+  };
+
+  const getSearchCity = async() => {
+    try{
+      const response = await axios.get('http://127.0.0.1:5001/search-city', {
+        params: { query: city },
+      });
+      console.log('Search City data:', response.data);
+      setSearchCity(response.data);
+      setError(null);
+    } catch(error){
+      setSearchCity(null);
+    }
+  };
+
+  const handleSearch = async () => {
+    await getWeather();
+    await getWeeklyForecast();
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="title">THE WEATHER APP</h1>
+        {/* Input and button to search for weather */}
+        <div className="city-search-container">
+          <input
+            type="text"
+            id="city-input"
+            placeholder="Enter a city..."
+            value={city}
+            onChange={(e) => {
+              setCity(e.target.value);
+            }}
+            onKeyDown={handleKeyDown}
+          />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+          <span className="material-symbols-outlined" onClick={handleSearch}>
+          search
+          </span>
+        </div>
+        
+        {/* Display error message if there's an error */}
+        {error && <p className="error-message">{error}</p>}
+        {/* Display weather data if available */}
+        {weather && ( <h2 className="location">{weather.location.name}, {weather.location.region}, {weather.location.country}</h2>)}
+        <div className="weather-card-container">
+          {weather && (
+              <div className="current-weather-card">
+                <h2>Current</h2>
+                <div className="current-temp">
+                  <h3 id="current-temp-value">{Math.round(weather.current.temp_c)}</h3>
+                  <h3 id="current-temp-celsius">°C</h3>
+                </div>
+                <p>{weather.current.condition.text}</p>
+                <p>Feels Like: {Math.round(weather.current.feelslike_c)}°C</p>
+                <p>Wind: {weather.current.wind_kph} km/h</p>
+                <p>Humidity: {weather.current.humidity}%</p>
+              </div>
+          )}
+          {weeklyForecast && (
+            <div className="weekly-forecast-cards">
+              <div className="day-one-weather">
+                <h2>Tomorrow</h2>
+                <div className="day-one-temp">
+                  <h3 id="day-one-value">{Math.round(weeklyForecast.forecast.forecastday[1].day.avgtemp_c)}</h3>
+                  <h3 id="day-one-celsius">°C</h3>
+                </div>
+                <p>{weeklyForecast.forecast.forecastday[1].day.condition.text}</p>
+                <p>Precipitation: {weeklyForecast.forecast.forecastday[1].day.daily_chance_of_rain}%</p>
+                <p>Max. Wind: {weeklyForecast.forecast.forecastday[1].day.maxwind_kph} km/h</p>
+                <p>Humidity: {weeklyForecast.forecast.forecastday[1].day.avghumidity}%</p>
+              </div>
+              <div className="day-two-weather">
+                <h2>Day After</h2>
+                <div className="day-two-temp">
+                  <h3 id="day-two-value">{Math.round(weeklyForecast.forecast.forecastday[2].day.avgtemp_c)}</h3>
+                  <h3 id="day-two-celsius">°C</h3>
+                </div>
+                <p>{weeklyForecast.forecast.forecastday[2].day.condition.text}</p>
+                <p>Precipitation: {weeklyForecast.forecast.forecastday[2].day.daily_chance_of_rain}%</p>
+                <p>Max. Wind: {weeklyForecast.forecast.forecastday[2].day.maxwind_kph} km/h</p>
+                <p>Humidity: {weeklyForecast.forecast.forecastday[2].day.avghumidity}%</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
