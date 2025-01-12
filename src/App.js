d1f9357c50dca7f34a87190a75166c75f43eb674
gayanrajapakshage <gayanr17@gmail.com>
2025-01-11 23:37:07 -0500

    Updated Backend URL

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   const getWeather = async () => {
     try {
-      const response = await axios.get('https://weather-app-ijxq.onrender.com/current-weather', {
+      const response = await axios.get('https://weather-app-xv4x.onrender.com/current-weather', {
         params: { city: city },
       });
       setWeather(response.data);
@@ -24,7 +24,7 @@ function App() {
 
   const getWeeklyForecast = async() => {
     try{
-      const response = await axios.get('https://weather-app-ijxq.onrender.com/weekly-forecast', {
+      const response = await axios.get('https://weather-app-xv4x.onrender.com/weekly-forecast', {
         params: { city: city },
       });
       console.log('Weekly forecast data:', response.data);
@@ -37,7 +37,7 @@ function App() {
 
   const getSearchCity = async() => {
     try{
-      const response = await axios.get('https://weather-app-ijxq.onrender.com/search-city', {
+      const response = await axios.get('https://weather-app-xv4x.onrender.com/search-city', {
         params: { query: city },
       });
       console.log('Search City data:', response.data);
